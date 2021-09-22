9ff6df95f06959dd3c80b540be3593a91a967e17
Alkin Maystorov <alkin971@gmail.com>
2021-09-22 13:17:07 +0200

    add connection to db, create genre model and routes

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const express = require('express');
 const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const DB = process.env.DB_URI;
 
-app.get('/', (req, res) => {
-    res.send('GET request to the homepage');
-});
+const genres = require('./routes/genres');
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log(`Connected to ${DB}`));
+
+app.use(express.json());
+app.use('/api/genres', genres);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
