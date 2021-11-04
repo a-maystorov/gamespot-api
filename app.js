0037c5e199c10bf52bc740334d6df8a3a0bc6144
Alkin Maystorov <alkin971@gmail.com>
2021-11-04 15:35:02 +0100

    ready for deployment

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const express = require('express');
 const app = express();
 
 require('./startup/logger')();
-
+require('./startup/cors')(app);
 require('./startup/routes')(app);
 require('./startup/prod')(app);
 require('./startup/db')();
