c72d1adcfacd5944b98cc754f38e7d02e4d3286b
Alkin Maystorov <alkin971@gmail.com>
2022-05-03 23:04:41 +0200

    add: tests for getting specific customer

--- tests/intergration/routes/customers.test.js
+++ tests/intergration/routes/customers.test.js
@@ -20,6 +20,16 @@ describe('/api/customers', () => {
   });
 
   describe('GET /', () => {
+    it('should return 401 if user is not logged in', async () => {
+      token = '';
+
+      const res = await request(server)
+        .get('/api/customers')
+        .set('x-auth-token', token);
+
+      expect(res.status).toBe(401);
+    });
+
     it('should return all customers if user is logged in', async () => {
       await Customer.collection.insertMany([
         { name: 'customer1', phone: '123456' },
@@ -39,7 +49,9 @@ describe('/api/customers', () => {
         res.body.some((c) => c.name === 'customer2' && c.phone === '654321')
       ).toBeTruthy();
     });
+  });
 
+  describe('GET /:id', () => {
     it('should return 401 if user is not logged in', async () => {
       token = '';
 
@@ -48,6 +60,37 @@ describe('/api/customers', () => {
         .set('x-auth-token', token);
 
       expect(res.status).toBe(401);
+    });
+
+    it('should return a customer if valid id is passed', async () => {
+      const customer = new Customer({ name: 'customer1', phone: '123456' });
+      await customer.save();
+
+      const res = await request(server)
+        .get('/api/customers/' + customer._id)
+        .set('x-auth-token', token);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('name', customer.name);
+      expect(res.body).toHaveProperty('phone', customer.phone);
+    });
+
+    it('should return 404 if invalid id is passed', async () => {
+      const res = await request(server)
+        .get('/api/customers/1')
+        .set('x-auth-token', token);
+
+      expect(res.status).toBe(404);
+    });
+
+    it('should return 404 if no customer with the given id exists', async () => {
+      const id = mongoose.Types.ObjectId();
+
+      const res = await request(server)
+        .get('/api/genres/' + id)
+        .set('x-auth-token', token);
+
+      expect(res.status).toBe(404);
     });
   });
 });
