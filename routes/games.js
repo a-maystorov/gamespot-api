15094142fb255bc241025aaf7d93eb36f38c2d09
Alkin Maystorov <alkin971@gmail.com>
2022-05-05 12:37:54 +0200

    add: test for getting specific game

--- routes/games.js
+++ routes/games.js
@@ -10,75 +10,77 @@ const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const validateObjectId = require('../middleware/validateObjectId');
 
-router.get('/', async(req, res) => {
-    const games = await Game.find().sort({ name: 1 });
-    res.send(games);
+router.get('/', async (req, res) => {
+  const games = await Game.find().sort({ name: 1 });
+  res.send(games);
 });
 
-router.post('/', auth, async(req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+router.get('/:id', [auth, validateObjectId], async (req, res) => {
+  const game = await Game.findById(req.params.id);
 
-    const genre = await Genre.findById(req.body.genreId);
-    if (!genre) return res.status(400).send('Invalid genre.');
+  if (!game)
+    return res.status(404).send('The game with the given ID was not found.');
 
-    const game = new Game({
-        title: req.body.title,
-        genre: {
-            _id: genre._id,
-            name: genre.name,
-        },
-        numberInStock: req.body.numberInStock,
-        dailyRentalRate: req.body.dailyRentalRate,
-        publishDate: moment().toJSON(),
-    });
-
-    await game.save();
-
-    res.send(game);
+  res.send(game);
 });
 
-router.put('/:id', [auth, validateObjectId], async(req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+router.post('/', auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    const genre = await Genre.findById(req.body.genreId);
-    if (!genre) return res.status(400).send('Invalid genre.');
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send('Invalid genre.');
 
-    const game = await Game.findByIdAndUpdate(
-        req.params.id, {
-            title: req.body.title,
-            genre: {
-                _id: genre._id,
-                name: genre.name,
-            },
-            numberInStock: req.body.numberInStock,
-            dailyRentalRate: req.body.dailyRentalRate,
-        }, { new: true }
-    );
+  const game = new Game({
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name,
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
+    publishDate: moment().toJSON(),
+  });
 
-    if (!game)
-        return res.status(404).send('The game with the given ID was not found.');
+  await game.save();
 
-    res.send(game);
+  res.send(game);
 });
 
-router.delete('/:id', [auth, admin, validateObjectId], async(req, res) => {
-    const game = await Game.findByIdAndRemove(req.params.id);
+router.put('/:id', [auth, validateObjectId], async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    if (!game)
-        return res.status(404).send('The game with the given ID was not found.');
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send('Invalid genre.');
 
-    res.send(game);
+  const game = await Game.findByIdAndUpdate(
+    req.params.id,
+    {
+      title: req.body.title,
+      genre: {
+        _id: genre._id,
+        name: genre.name,
+      },
+      numberInStock: req.body.numberInStock,
+      dailyRentalRate: req.body.dailyRentalRate,
+    },
+    { new: true }
+  );
+
+  if (!game)
+    return res.status(404).send('The game with the given ID was not found.');
+
+  res.send(game);
 });
 
-router.get('/:id', [auth, validateObjectId], async(req, res) => {
-    const game = await Game.findById(req.params.id);
+router.delete('/:id', [auth, admin, validateObjectId], async (req, res) => {
+  const game = await Game.findByIdAndRemove(req.params.id);
 
-    if (!game)
-        return res.status(404).send('The game with the given ID was not found.');
+  if (!game)
+    return res.status(404).send('The game with the given ID was not found.');
 
-    res.send(game);
+  res.send(game);
 });
 
 module.exports = router;
--- tests/intergration/routes/games.test.js
+++ tests/intergration/routes/games.test.js
@@ -41,4 +41,76 @@ describe('/api/games', () => {
       expect(res.body.some((g) => g.dailyRentalRate === 4)).toBeTruthy();
     });
   });
+
+  describe('GET /:id', () => {
+    let token;
+
+    beforeEach(() => {
+      token = new User().generateAuthToken();
+      return token;
+    });
+
+    it('should return 401 if user is not logged in', async () => {
+      const game = new Game({
+        title: 'game1',
+        genre: { name: 'genre1' },
+        numberInStock: 1,
+        dailyRentalRate: 2,
+      });
+      await game.save();
+
+      token = '';
+
+      const res = await request(server)
+        .get('/api/games/' + game._id)
+        .set('x-auth-token', token);
+
+      expect(res.status).toBe(401);
+    });
+
+    it('should return a game if valid id is passed', async () => {
+      const game = new Game({
+        title: 'game1',
+        genre: { name: 'genre1' },
+        numberInStock: 1,
+        dailyRentalRate: 2,
+      });
+      await game.save();
+
+      const res = await request(server)
+        .get('/api/games/' + game._id)
+        .set('x-auth-token', token);
+
+      expect(res.status).toBe(200);
+      expect(Object.keys(res.body)).toEqual(
+        expect.arrayContaining([
+          'title',
+          'genre',
+          'numberInStock',
+          'dailyRentalRate',
+        ])
+      );
+      expect(res.body).toHaveProperty('title', game.title);
+      expect(res.body).toHaveProperty('numberInStock', game.numberInStock);
+      expect(res.body).toHaveProperty('dailyRentalRate', game.dailyRentalRate);
+    });
+
+    it('should return 404 if invalid id is passed', async () => {
+      const res = await request(server)
+        .get('/api/games/1')
+        .set('x-auth-token', token);
+
+      expect(res.status).toBe(404);
+    });
+
+    it('should return 404 if no game with the given id exists', async () => {
+      const id = mongoose.Types.ObjectId();
+
+      const res = await request(server)
+        .get('/api/games/' + id)
+        .set('x-auth-token', token);
+
+      expect(res.status).toBe(404);
+    });
+  });
 });
