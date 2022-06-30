64210c25304fbba9e106294fadcb2ba787fd123a
Alkin Maystorov <alkin971@gmail.com>
2022-06-30 12:02:59 +0200

    set x-auth-token in login

--- routes/auth.js
+++ routes/auth.js
@@ -6,27 +6,30 @@ const bcrypt = require('bcrypt');
 
 const { User } = require('../models/user');
 
-router.post('/', async(req, res) => {
-    const { error } = validateAuth(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+router.post('/', async (req, res) => {
+  const { error } = validateAuth(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    let user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).send('Invalid email or password');
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Invalid email or password');
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).send('Invalid email or password.');
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-    const token = user.generateAuthToken();
-    res.send(token);
+  const token = user.generateAuthToken();
+  res
+    .header('x-auth-token', token)
+    .header('access-control-expose-headers', 'x-auth-token')
+    .send(token);
 });
 
 function validateAuth(req) {
-    const schema = Joi.object({
-        email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(255).required(),
-    });
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  });
 
-    return schema.validate(req);
+  return schema.validate(req);
 }
 
 module.exports = router;
