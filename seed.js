d060fe3062460220a31c857b91c5e2820f3c6d7f
a-maystorov <alkin971@gmail.com>
2024-07-16 16:50:44 +0200

    refactor: connect seed file to main db

--- seed.js
+++ seed.js
@@ -1,44 +1,47 @@
-const { Genre } = require('./models/genre');
-const { Game } = require('./models/game');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const { Genre } = require("./models/genre");
+const { Game } = require("./models/game");
+const mongoose = require("mongoose");
 
 const data = [
   {
-    name: 'MMORPG',
+    name: "MMORPG",
     games: [
-      { title: 'World of Warcraft', numberInStock: 5, dailyRentalRate: 2 },
-      { title: 'Final Fantasy XIV', numberInStock: 10, dailyRentalRate: 2 },
-      { title: 'Blade and Soul', numberInStock: 15, dailyRentalRate: 2 },
+      { title: "World of Warcraft", numberInStock: 5, dailyRentalRate: 2 },
+      { title: "Final Fantasy XIV", numberInStock: 10, dailyRentalRate: 2 },
+      { title: "Blade and Soul", numberInStock: 15, dailyRentalRate: 2 },
     ],
   },
   {
-    name: 'MOBA',
+    name: "MOBA",
     games: [
-      { title: 'League of Legends', numberInStock: 5, dailyRentalRate: 2 },
-      { title: 'Dota 2', numberInStock: 10, dailyRentalRate: 2 },
-      { title: 'Heroes of the Storm', numberInStock: 15, dailyRentalRate: 2 },
+      { title: "League of Legends", numberInStock: 5, dailyRentalRate: 2 },
+      { title: "Dota 2", numberInStock: 10, dailyRentalRate: 2 },
+      { title: "Heroes of the Storm", numberInStock: 15, dailyRentalRate: 2 },
     ],
   },
   {
-    name: 'RPG',
+    name: "RPG",
     games: [
-      { title: 'Dark Souls', numberInStock: 5, dailyRentalRate: 2 },
-      { title: 'Bloodbourne', numberInStock: 10, dailyRentalRate: 2 },
-      { title: 'Elden Ring', numberInStock: 15, dailyRentalRate: 2 },
+      { title: "Dark Souls", numberInStock: 5, dailyRentalRate: 2 },
+      { title: "Bloodbourne", numberInStock: 10, dailyRentalRate: 2 },
+      { title: "Elden Ring", numberInStock: 15, dailyRentalRate: 2 },
     ],
   },
   {
-    name: 'FPS',
+    name: "FPS",
     games: [
-      { title: 'Counter Strike', numberInStock: 5, dailyRentalRate: 2 },
-      { title: 'Valorant', numberInStock: 10, dailyRentalRate: 2 },
-      { title: 'Quake', numberInStock: 15, dailyRentalRate: 2 },
+      { title: "Counter Strike", numberInStock: 5, dailyRentalRate: 2 },
+      { title: "Valorant", numberInStock: 10, dailyRentalRate: 2 },
+      { title: "Quake", numberInStock: 15, dailyRentalRate: 2 },
     ],
   },
 ];
 
+const DB = process.env.DB_URI;
+
 async function seed() {
-  await mongoose.connect(`mongodb://localhost:27017/gamespot_tests`);
+  await mongoose.connect(`${DB}`);
 
   await Game.deleteMany({});
   await Genre.deleteMany({});
@@ -54,7 +57,7 @@ async function seed() {
 
   mongoose.disconnect();
 
-  console.info('Done!');
+  console.info("Done!");
 }
 
 seed();
